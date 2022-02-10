29179431dad590034f064f165dc0e691efea0c95
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2022-02-10 20:43:45 +0200

    modif

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
-$servernae="db";
-$username="MhammedAlsadi";
-$password="Alsaad525415";
-$dbname="MoammedAlsaadi";
-$conn = new mysqli($servernae,$username,$password,$dbname);
+$servername="db";
+$username="MohammedAlsaadi";
+$password="password1";
+$dbname="MohammedAlsaadi";
+$conn = new mysqli($servername,$username,$password,$dbname);
 if($conn ->connect_error)
-die("conection failed:".$conn->cnnesct_error);
+die("conection failed:".$conn->connect_error);
 
 ?>
