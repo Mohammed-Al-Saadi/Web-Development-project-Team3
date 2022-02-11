3c178172bcfe90042a4079c25e877f3eeebfb95b
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2022-02-12 00:07:54 +0200

    welcome

--- Userform.php
+++ Userform.php
@@ -1,8 +1,38 @@
 <?php
 include 'header.php';
 
-?>
-<?php
-include 'footer.php';
+
+
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+</head>
+<body>
+    <div class="row row1" >
+        <div class="col-lg-6">
+    <?php
+include 'connect.php';
+
+$sql=" SELECT * FROM Login WHERE id=(SELECT max(id) FROM Login)";
+$result = $conn -> query($sql);
+if($result ->num_rows > 0) {
+    echo "welcome ";
+while($row = $result ->fetch_assoc()){
+
+    echo  $row['Firstname'] ;
+}
+}
+    ?>
+    
+    </div>
+    </div>
+</body>
+</html>
