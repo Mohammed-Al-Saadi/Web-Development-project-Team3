5fbf4e628e3ba1e1414b6ff237bc97400db718f6
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2022-02-16 12:01:53 +0200

    login and out

--- connect.php
+++ connect.php
@@ -3,6 +3,8 @@ $servername="db";
 $username="MohammedAlsaadi";
 $password="password1";
 $dbname="MohammedAlsaadi";
+
+
 $conn = new mysqli($servername,$username,$password,$dbname);
 if($conn ->connect_error)
 die("conection failed:".$conn->connect_error);
