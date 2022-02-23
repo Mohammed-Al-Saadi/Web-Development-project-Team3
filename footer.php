78557c796ac6b64f5d17f0b786c07437de6c65b3
collinz007 <97731624+collinz007@users.noreply.github.com>
2022-02-23 11:53:48 +0100

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+<style>
+      
+    
+      .footer
+    
+    {
+   
+       
+        margin-top: 100px;
+        background-color: rgb(93, 175, 223);
+    }
+
+.copyright{
+
+ margin-left: 20px;
+
+}
+.copyright p
+{
+margin-left: 5px;
+
+}
+.footer img {
+width: 120px;
+height: auto;
+}
+@media(max-width: 1000px){
+    .copyright{
+        margin: 0px;
+    }
+}
+</style>
+
+</head>
+<body>
+<div class="footer">
+      <div class="row">
+          <div class="col-sm-3">
+
+      <div class="copyright">
+          <img src="Picturekaisa.png" alt="logo">
+           <p>&copy; 2022 wecare<br><a href="FeedbackForm.php">Give feedback</a></p>
+          </div>
+
+      </div>
+      <div class="col-sm-3">
+          <div class="aboutus">
+              <p><b>Contact us</b><br>Näsilinnänkatu 20<br>33100 Tampere<br>Tel-<a href="">045 555 334</a><br><a href="#">wecare@company.com</a></p>
+          </div>
+
+      </div>
+      <div class="col-sm-3">
+          <div class="opening">
+          <b>Opening hours</b>
+          <p>Mon-Fri 8am-6pm</p>
+
+         
+      </div>
+      </div>
+
+
+      <div class="col-sm">
+
+      <div class="followus">
+          <p><b>Follow us</b><br>
+              <a href="#" class="fa fa-facebook"></a> 
+              <a href="#" class="fa fa-instagram"></a>
+              <a href="#" class="fa fa-youtube"></a>
+              <a href="#" class="fa fa-twitter"></a> 
+
+
+              
+          </p>
+
+      </div>
+
+  
+  </div>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
