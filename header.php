af20d3962fd60ef0bbf9ff725b24245d0c246669
collinz007 <97731624+collinz007@users.noreply.github.com>
2022-03-01 06:49:02 +0200

    signUpForm
    i have added the signupForm to the login

---
+++ header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+    <title>Document</title>
+<style>
+    .navbar-brand img {
+    width: 180px;
+} 
+.container-fluid {
+font-family: Arial, Helvetica, sans-serif;
+}
+.nav-item a{
+margin-left: 110px;
+font-weight: bold;
+}
+.d-flex{
+    margin-right: 30px;
+}
+.navbar{
+background-color: rgb(93, 175, 223);
+}
+.dropdown-menu{
+    background-color: rgb(93, 175, 223);
+    border: none;
+}
+ .btn-outline-success{
+    background-color: white;
+    color: black;
+}
+.container-fluid {
+font-family: Arial, Helvetica, sans-serif;
+}
+.footer
+    
+    {
+   
+       
+        margin-top: 100px;
+        background-color: rgb(93, 175, 223);
+    }
+ .btn-outline-success a{
+   text-decoration: none;
+   color: black;
+ }
+.copyright{
+ margin-left: 20px;
+}
+.copyright p
+{
+margin-left: 5px;
+}
+.footer img {
+width: 120px;
+height: auto;
+}
+@media(max-width: 1000px){
+    .copyright{
+        margin: 0px;
+    }
+@media(max-width: 1000px){
+ 
+    .navbar a{
+        margin-left: 10px;
+   }
+   .navbar-nav a{
+border-bottom: 3px inset;
+border-width: 1px;
+border-bottom-color: rgb(255, 228, 204);
+}
+}
+
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index1.php"><img src="Picturekaisa.png"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link "  href="index2.php">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index3.php">About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Appointment</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Doctors
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Doctor1</a></li>
+                  <li><a class="dropdown-item" href="#">Doctor2</a></li>
+                </ul>
+              </li>
+             
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit"><a href="login.php">Login</a></button>
+            </form>
+          </div>
+        </div>
+      </nav>
+     
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
